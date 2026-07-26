a5ba9e1df0b59408af3998f6acc8a858f672314e
mreznmx <haloc43614@barumart.com>
2026-07-26 23:57:41 +0400

    Change redirect URL in main.ts

---
+++ main.ts
@@ -0,0 +1,22 @@
+Deno.serve(() => {
+  return new Response(`
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Redirecting...</title>
+<script>
+setTimeout(() => {
+    window.location.href = "https://goat-ae.com/NSP";
+}, 1000);
+</script>
+</head>
+<body>
+</body>
+</html>
+`, {
+    headers: {
+      "content-type": "text/html"
+    }
+  });
+});
